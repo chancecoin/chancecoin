b20f16a230af3a08eb06bad21be2a88afad9ec47
Zack Coburn <chancecoin@chancecoin.com>
2014-03-23 15:32:19 -0500

    update

---
+++ generate_chancecoin.com.sh
@@ -0,0 +1,20 @@
+cp templates/template.html templates/template_temp.html
+cp templates/template_chancecoin.com.html templates/template.html
+cp templates/wallet.html templates/wallet_temp.html
+cp templates/wallet_chancecoin.com.html templates/wallet.html
+cp templates/casino.html templates/casino_temp.html
+cp templates/casino_chancecoin.com.html templates/casino.html
+python3.2 server.py &
+sleep 5
+rm -Rf chancecoin.com
+mkdir chancecoin.com
+cp -r static chancecoin.com/
+cp .htaccess chancecoin.com/
+wget -O chancecoin.com/index.html http://0.0.0.0:8080/
+wget -O chancecoin.com/participate.html http://0.0.0.0:8080/participate
+wget -O chancecoin.com/casino.html http://0.0.0.0:8080/casino
+wget -O chancecoin.com/wallet.html http://0.0.0.0:8080/wallet
+killall -9 Python
+mv templates/template_temp.html templates/template.html
+mv templates/wallet_temp.html templates/wallet.html
+mv templates/casino_temp.html templates/casino.html
